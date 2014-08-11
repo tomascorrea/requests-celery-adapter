9f77c8a7214d23ffe47e45523489fdeb21cbe6a2
Diogo Fernandes <diogo@advertiser.com.br>
2014-08-11 15:55:16 -0300

    send class rewrote

--- rca/adapters.py
+++ rca/adapters.py
@@ -6,7 +6,7 @@ from requests import Response
 from requests.compat import urlparse, StringIO
 from requests.adapters import BaseAdapter
 from requests.hooks import dispatch_hook
-from kombu import Connection, Exchange, Queue
+from kombu import Connection, Exchange, Queue, BrokerConnection
 import datetime
 
 
@@ -36,30 +36,39 @@ class CeleryAdapter(BaseAdapter):
     def send(self, request, **kwargs):
 
         with Connection(request.url) as conn:
+            return self._send(conn, request, **kwargs)
 
-            simple_queue = conn.SimpleQueue(
-                request.headers.get('queue', 'default')
-            )
 
-            message = {"id": uuid.uuid4().hex,
-                       "task": request.headers['task'],
-                       "args": [],
-                       "kwargs": json.loads(request.body),
-                       "retries": request.headers.get('retries', 0),
-                       "eta": datetime.datetime.now().isoformat()}
+    def _send(self, conn, request, **kwargs):
 
-            simple_queue.put(message)
-            simple_queue.close()
+        simple_queue = conn.SimpleQueue(
+            request.headers.get('queue', 'default')
+        )
+
+        message = {"id": uuid.uuid4().hex,
+                   "task": request.headers['task'],
+                   "args": [],
+                   "kwargs": json.loads(request.body),
+                   "retries": request.headers.get('retries', 0),
+                   "eta": datetime.datetime.now().isoformat()}
+
+        simple_queue.put(message)
+        simple_queue.close()
 
         data = json.dumps({})
 
         return build_response(request, data, 200, 'ascii')
 
 
+
 class AmqpCeleryAdapter(CeleryAdapter):
     pass
 
 class SQSCeleryAdapter(CeleryAdapter):
-    pass
+    def send(self, request, **kwargs):
+        with BrokerConnection(request.url, transport_options = {'region': 'sa-east-1'}) as conn:
+            return self._send(conn, request, **kwargs)
+
+
 
 
