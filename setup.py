6cf1050442196069e5bc37d753d92cafe802cf53
Tomas Correa <tomas.correa@gmail.com>
2015-07-02 10:38:16 -0300

    pin requirements version

--- setup.py
+++ setup.py
@@ -4,25 +4,23 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md')) as f:
-  README = f.read()
+    README = f.read()
 
-requires = [
-            'kombu',
-            'requests',
-            'click',
-            'boto',
-    ]
+requires = ['kombu==3.0.26',
+            'requests==2.3.0',
+            'click==3.1',
+            'boto==2.30.0',
+            ]
 
 setup(name='celery_adapter',
       version='0.1',
       description='Rating Analytics Celery Client',
       # long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Celery",
-        "Topic :: Internet :: Worker",
-        "Topic :: Internet :: Worker :: CLI :: Application",
-        ],
+      classifiers=["Programming Language :: Python",
+                   "Framework :: Celery",
+                   "Topic :: Internet :: Worker",
+                   "Topic :: Internet :: Worker :: CLI :: Application",
+                   ],
       author='Diog Fernandes',
       author_email='diogo@geru.com.br',
       url='',
@@ -32,10 +30,10 @@ setup(name='celery_adapter',
       zip_safe=False,
       test_suite='requests-celery-adapter',
       install_requires=requires,
-      entry_points = """\
+      entry_points="""\
       [paste.app_factory]
       main = rca:main
       [console_scripts]
       celery-send-task = rca.scripts.cli:send_task
       """,
-)
+      )
