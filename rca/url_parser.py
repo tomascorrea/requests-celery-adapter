06972fc9f5fe3bf198f9dc3bb865ebf3198d5a27
Daniel Debonzi <debonzi@gmail.com>
2017-09-24 19:10:07 -0300

    Using url anchor to identify queue.

--- rca/url_parser.py
+++ rca/url_parser.py
@@ -3,44 +3,22 @@ import six
 import posixpath
 
 
-"""
-    core_broker_url = redis://broker-production.erdvhn.0001.sae1.cache.amazonaws.com:6379/1
-    queue = geru.loan
-
-    headers = {'task': 'api.v1.loan.soft_deny', 'queue': queue}
-
-    data = {
-        'loan_uuid': loan_uuid,
-        'score': geru_score,
-        'rate': operation_rate,
-        'interest': interest,
-        'capacity': capacity['monthly_supported_value'],
-        'new_principal': new_principal,
-        'new_instalment_number': length
-    }
-
-    requests.post(
-        core_broker_url, headers=headers,
-        data=json.dumps(data)
-    )
-
-
-    redis://broker-production.erdvhn.0001.sae1.cache.amazonaws.com:6379/1/api.v1.loan.soft_deny/geru.loan
-
-
-"""
-
-
 class Parser(object):
     def __init__(self, url):
+        """
+
+        :param url: formated url
+         -> Format: <scheme>://<broker url>/<task_name>#queue
+            e.g: redis://myredis.example.com/process_request#process_queue
+        """
         self._parsed = six.moves.urllib.parse.urlparse(url)
-        self._broker_path, self._task, self._queue = self._path_split()
+        self._broker_path, self._task = self._path_split()
+        self._queue = self._parsed.fragment
 
     def _path_split(self):
-        path, queue = posixpath.split(self._parsed.path)
-        broker_path, task = posixpath.split(path)
+        broker_path, task = posixpath.split(self._parsed.path)
         broker_path = broker_path[1:] if broker_path.startswith(posixpath.sep) else broker_path
-        return broker_path, task, queue
+        return broker_path, task
 
     @property
     def queue(self):
@@ -52,6 +30,6 @@ class Parser(object):
 
     @property
     def broker_url(self):
-        # Avoids posixpath.join from append '/' at the end if self._broker_path is an empty string.
+        # Avoid posixpath.join from append '/' at the end if self._broker_path is an empty string.
         _endpoint = posixpath.join(self._parsed.netloc, self._broker_path) if self._broker_path else self._parsed.netloc
         return "{}://{}".format(self._parsed.scheme, _endpoint)
